c5d0b10b8055db737601963dbd931a8f1147154d
Samara Dias da Silva <marcoscz329@gmail.com>
2026-02-02 16:08:37 -0300

    Enviando exercicio

---
+++ src/ExercicioSenha.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ExercicioSenha {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite sua senha: ");
+        String senha = leitura.nextLine();
+
+        try {
+            validarSenha(senha);
+            System.out.println("Senha válida. Acesso permitido.");
+        } catch (SenhaInvalidaException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+    private static void validarSenha (String senha){
+        if (senha.length() < 8){
+            throw new SenhaInvalidaException("A senha deve ter pelo menos 8 caracteres.");
+        }
+
+    }
+}
